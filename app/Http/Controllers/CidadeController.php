ad1b3fad6384a33f6b33747d665f9b43b145eb9f
gMoraes1 <gustavo.correiademoraes2006@gmail.com>
2024-10-07 21:49:25 -0300

    primeiro atualização

---
+++ app/Http/Controllers/CidadeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cidade;
+use Illuminate\Support\Facades\Redirect;
+
+class CidadeController extends Controller
+{
+    public function MostrarHome()
+    {
+        return view('homecidade');
+    }
+
+    public function MostrarCadastroCidade()
+    {
+        return view('cadastrocidade');
+    }
+
+    public function CadastrarCidade(Request $request)
+    {
+        $registros = $request->validate([
+            'nomeCidade' => 'string|required',
+            'numeroPopulacao' => 'integer|required',
+            'silgaCidade' => 'string|required',
+            'nomeVereador' => 'string|required'
+        ]);
+
+        Cidade::create($registros);
+        return Redirect::route('home-cidade');
+    }
+
+    public function Destroy(Cidade $id)
+    {
+        $id->delete();
+        return Redirect::route('home-cidade');
+    }
+    public function Update(Cidade $id, Request $request)
+    {
+        $registros = $request->validate([
+            'nomeCidade' => 'string|required',
+            'numeroPopulacao' => 'integer|required',
+            'siglaCidade' => 'string|required',
+            'nomeVereador' => 'string|required'
+        ]);
+        $id->fill($registros);
+        $id->save();
+
+        return Redirect::route('home-cidade');
+    }
+    public function MostrarCidadeCodigo(Cidade $id)
+    {
+        return view("alteracidade", ['registrosCidade' => $id]);
+    }
+
+    public function MostrarCidadeNome(Request $request)
+    {
+        $registros = Cidade::query();
+        $registros->when($request->nomeCidade, function ($query, $valor) {
+            $query->where('nomeCidade', 'like', '%' . $valor . '%');
+        });
+        $todosRegistros = $registros->get();
+        return view('listacidade', ['registrosCidade' => $todosRegistros]);
+    }
+}
